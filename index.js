6a0ec49da27979d392cc64696a1531956606e9c4
Adam Romero <aromero0904@gmail.com>
2022-12-13 01:21:49 -0500

    commit tree 10c8984483ef27d431a1f7e0adbc0d950db13527

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
 
-// Write your code here
+
+const random = Math.floor(Math.random()*10) + 1;
+
+
+const num3 = 5
+const num4= 24
+const mod = Math.floor(num4 / num3);
+
+const max = Math.max(1,6,3,5,7,20);
